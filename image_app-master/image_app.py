6a846c84d9c18f9d86e8630a1f0377f91c5aad6f
Sachin Dalvi <dalvisv9@gmail.com>
2021-02-05 11:42:02 -0800

    Loop through YouTube video

--- image_app-master/image_app.py
+++ image_app-master/image_app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-import pafy
+import pafy #retrive metadata 
 import cv2
 
 from skimage.metrics import structural_similarity as ssim
@@ -21,6 +21,7 @@ def get_video_frames(url):
 	cam = cv2.VideoCapture(best.url)
 	# number of frames in video
 	num_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+	#fps = cam.get(cv2.CAP_PROP_FPS) # Gets the frames per second
 	return cam, num_frames
 
 ####################################################
@@ -51,7 +52,7 @@ def show_2_imgs(target, img=None):
 	axes[0].set_title("Target")
 	if img is not None:
 		axes[1].imshow(img, )
-		axes[1].set_title("Frame")
+		axes[1].set_title("Match")
 	st.pyplot(fig)
 
 #############################################################
@@ -67,32 +68,44 @@ if img_target is not None:
 	# convert target image to RGB and put in numpy array format.
 	img_target_RGB = np.array(img_target.convert("RGB"))
 
-	# get video frames from youtube
-	# video URL
-	url = "https://www.youtube.com/watch?v=VvL5Q7YVyWM"
-	frame_generator, num_frames = get_video_frames(url)
-	st.write(f"Video url: {url} \n\n **Total in video: {num_frames} frames**")
+	# get video frames from youtube directely from YouTube based on the tags such as COC *** future updated ***
+	# video URL for selected videos only use two videos for demos. 
+	url = ["https://www.youtube.com/watch?v=VvL5Q7YVyWM","https://www.youtube.com/watch?v=VvL5Q7YVyWM"]
 
-	# LOOP to search for match frame
-	threshold = 0.8
-	found = False
-	for i in range(num_frames):
-		# current frame
-		_, frame = frame_generator.read()
-		# convert current frame from BGR to RGB format
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		# check if the current frame is similar to the target above a threshold
-		# Here I only check every 10 frames to save time.
-		if i%20==0 and struc_sim(img_target_RGB, frame) >= threshold:
-			# show the ssim value
-			st.text(f'**Found match !**\
-					\nFrame number: {i+1}\
-					\nSSIM: {struc_sim(img_target_RGB, frame):.4}')
-			found = True
-			# plot the target and frame side by side.
-			show_2_imgs(img_target_RGB, frame)
+	# loop through video links
+	for url in url:
+		frame_generator, num_frames = get_video_frames(url)
+		st.write(f"Video url: {url} \n\n **Total in video: {num_frames} frames**")
+
+		# LOOP to search for match frame
+		threshold = 0.65
+		found = False
+		for i in range(num_frames):
+			# current frame
+			_, frame = frame_generator.read()
+			# convert current frame from BGR to RGB format
+			#frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+			# Future release How to extract one frame of a video every N seconds to an image?
+			# check if the current frame is similar to the target above a threshold
+			# Here I only check every 10 frames to save time.
+			#if i%20==0 and struc_sim(img_target_RGB, frame) >= threshold:
+			if struc_sim(img_target_RGB, frame) >= threshold:
+				# show the ssim value
+				st.write(f"Video url: {url} \n\n **Total in video: {num_frames} frames**")
+				st.text(f'**Found match !**\
+						\nFrame number: {i+1}\
+						\nSSIM: {struc_sim(img_target_RGB, frame):.4}')
+				found = True
+				# plot the target and frame side by side.
+				show_2_imgs(img_target_RGB, frame)
+				break
+			i +=60 # i.e. at 30 fps, this advances one second
+		if found == True:
 			break
+		else:
+			st.write("Target Not Found in this video")
+
 	# if no match found:
 	if not found:
-		st.write("### Target image NOT from this video.")
+		st.write("### Target image is NOT FOUND in avialable video links")
 		show_2_imgs(img_target_RGB, None)
